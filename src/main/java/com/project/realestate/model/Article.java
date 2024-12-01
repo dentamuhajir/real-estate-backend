9e83410a8f5681f794f1ac6b2c4feffbe5c371ca
Denta Muhajir <dentamuhajir@gmail.com>
2024-12-01 13:12:38 +0700

    Update Article Model by added validation

--- src/main/java/com/project/realestate/model/Article.java
+++ src/main/java/com/project/realestate/model/Article.java
@@ -4,7 +4,7 @@ package com.project.realestate.model;
 import jakarta.persistence.*;
 import lombok.Data;
 
-import java.util.Date;
+import java.time.LocalDateTime;
 
 @Data
 @Entity
@@ -13,11 +13,11 @@ public class Article {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
-    @Column(name = "title")
+    @Column(name = "title", nullable = false)
     private String title;
-    @Column(name = "category")
+    @Column(name = "category", nullable = false)
     private String category;
-    @Column(name = "content_body")
+    @Column(name = "content_body", nullable = false)
     private String contentBody;
     @Column(name = "photo")
     private String photo;
@@ -26,7 +26,11 @@ public class Article {
     @Column(name = "photographer")
     private String photographer;
     @Column(name = "published_date")
-    private Date publishedDate;
-    @Column(name = "is_published")
+    private LocalDateTime publishedDate;
+    @Column(name = "is_published", nullable = false)
     private Boolean isPublished;
+    @Column(name= "created_at", updatable = false)
+    private LocalDateTime createdAt;
+    @Column(name="updated_at")
+    private  LocalDateTime updatedAt;
 }

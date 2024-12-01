7ce7777999d586e8d93faaf2656f277670e4d79b
Denta Muhajir <dentamuhajir@gmail.com>
2024-12-01 10:38:49 +0700

    Refactor naming project

--- src/main/java/com/project/realestate/RealEstateApplication.java
+++ src/main/java/com/project/realestate/RealEstateApplication.java
@@ -1,4 +1,4 @@
-package com.project.real_estate;
+package com.project.realestate;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/project/realestate/RealEstateApplicationTests.java
+++ src/test/java/com/project/realestate/RealEstateApplicationTests.java
@@ -1,4 +1,4 @@
-package com.project.real_estate;
+package com.project.realestate;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;

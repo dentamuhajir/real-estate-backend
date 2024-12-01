4f2c52436a8beb2617467737c85d80cf60edd47f
Denta Muhajir <dentamuhajir@gmail.com>
2024-12-01 12:49:05 +0700

    Update article model to implement GenerationType strategy

--- src/main/java/com/project/realestate/model/Article.java
+++ src/main/java/com/project/realestate/model/Article.java
@@ -1,25 +1,32 @@
 package com.project.realestate.model;
 
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.Data;
 
 import java.util.Date;
 
 @Data
 @Entity
-@Table(name = "articles")
+@Table(name = "articles", schema = "public")
 public class Article {
     @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+    @Column(name = "title")
     private String title;
+    @Column(name = "category")
     private String category;
+    @Column(name = "content_body")
     private String contentBody;
+    @Column(name = "photo")
+    private String photo;
+    @Column(name = "author")
     private String author;
+    @Column(name = "photographer")
     private String photographer;
+    @Column(name = "published_date")
     private Date publishedDate;
-    private  Boolean isPublished;
+    @Column(name = "is_published")
+    private Boolean isPublished;
 }

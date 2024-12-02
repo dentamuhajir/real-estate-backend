199c9f96449dd53a21d5bcac71d8f180a2b2e394
Denta Muhajir <dentamuhajir@gmail.com>
2024-12-02 18:06:49 +0700

    Add Java Faker to generate / seeding random data

--- src/main/java/com/project/realestate/RealEstateApplication.java
+++ src/main/java/com/project/realestate/RealEstateApplication.java
@@ -1,5 +1,6 @@
 package com.project.realestate;
 
+import com.github.javafaker.Faker;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -8,6 +9,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class RealEstateApplication implements CommandLineRunner {
 	public void run(String args[]) throws Exception
 	{
+		Faker faker = new Faker();
+		for (int i = 0; i < 10; i++) { // Generate 10 articles
+			System.out.println("title : " + faker.book().title());
+			System.out.println("content : " + faker.lorem().paragraph());
+			System.out.println("author : " + faker.name().fullName());
+			System.out.println("image : " + "https://picsum.photos/seed/" + faker.number().numberBetween(1, 1001) + "/600/400");
+		}
 		System.out.println("Command Line Runner Execute");
 	}
 

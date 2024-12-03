9ae23ab09625ebb7c1e4fe5b5cd7d78034429aec
Denta Muhajir <dentamuhajir@gmail.com>
2024-12-03 23:07:47 +0700

    Save seeding article data into article repository

--- src/main/java/com/project/realestate/RealEstateApplication.java
+++ src/main/java/com/project/realestate/RealEstateApplication.java
@@ -1,22 +1,51 @@
 package com.project.realestate;
 
 import com.github.javafaker.Faker;
+import com.project.realestate.model.Article;
+import com.project.realestate.repository.ArticleRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
 @SpringBootApplication
 public class RealEstateApplication implements CommandLineRunner {
+
+	private ArticleRepository articleRepository;
+
+	public RealEstateApplication(ArticleRepository articleRepository) {
+		this.articleRepository = articleRepository;
+	}
 	public void run(String args[]) throws Exception
 	{
 		Faker faker = new Faker();
-		for (int i = 0; i < 10; i++) { // Generate 10 articles
-			System.out.println("title : " + faker.book().title());
-			System.out.println("content : " + faker.lorem().paragraph());
-			System.out.println("author : " + faker.name().fullName());
-			System.out.println("image : " + "https://picsum.photos/seed/" + faker.number().numberBetween(1, 1001) + "/600/400");
+
+		List<String> genres = new ArrayList<>();
+		for (int i = 0; i < 3; i++) {
+			genres.add(faker.lorem().word());
 		}
-		System.out.println("Command Line Runner Execute");
+
+		for (int i = 0; i < 60; i++) {
+			Article article = new Article();
+			article.setTitle(faker.lorem().sentence(4));
+			String randomCategory = genres.get(faker.number().numberBetween(0, genres.size()));
+			article.setCategory(randomCategory);
+			article.setContentBody(faker.lorem().paragraph());
+			article.setAuthor(faker.name().fullName());
+			article.setPhotographer(faker.funnyName().name());
+			article.setPhoto("https://picsum.photos/seed/" + faker.number().numberBetween(1, 1001) + "/600/400");
+			Date randomDate = faker.date().past(20 * 365, TimeUnit.DAYS);
+			LocalDateTime publishedDate = LocalDateTime.ofInstant(randomDate.toInstant(), ZoneId.systemDefault());
+			article.setPublishedDate(publishedDate);
+			article.setIsPublished(true);
+			articleRepository.save(article);
+		}
 	}
 
 	public static void main(String[] args) {

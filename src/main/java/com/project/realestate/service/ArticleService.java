59de214beca39794dcfc221ede3cec1d13a0bd1e
Denta Muhajir <dentamuhajir@gmail.com>
2024-12-13 08:18:55 +0700

    Get service from controller using autowired

---
+++ src/main/java/com/project/realestate/service/ArticleService.java
@@ -0,0 +1,10 @@
+package com.project.realestate.service;
+
+import com.project.realestate.model.Article;
+
+import java.util.List;
+
+
+public interface ArticleService {
+    public String getHeadline();
+}

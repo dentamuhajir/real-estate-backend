4b9dd1f140d65581cd2c5dd7e9814fc894f4f04d
Denta Muhajir <dentamuhajir@gmail.com>
2024-12-13 17:20:32 +0700

    Create service for serve article headline

--- src/main/java/com/project/realestate/repository/ArticleRepository.java
+++ src/main/java/com/project/realestate/repository/ArticleRepository.java
@@ -2,6 +2,12 @@ package com.project.realestate.repository;
 
 import com.project.realestate.model.Article;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
+import java.util.List;
+@Repository
 public interface ArticleRepository extends JpaRepository<Article, Long> {
+    @Query(value = "SELECT id, title, author, category, content_body, photo, photographer, published_date, is_published, created_at, updated_at FROM articles ORDER BY RAND() LIMIT 2", nativeQuery = true)
+    List<Article> fetchHeadline();
 }
--- src/main/java/com/project/realestate/service/impl/ArticleServiceImpl.java
+++ src/main/java/com/project/realestate/service/impl/ArticleServiceImpl.java
@@ -3,14 +3,19 @@ package com.project.realestate.service.impl;
 import com.project.realestate.model.Article;
 import com.project.realestate.repository.ArticleRepository;
 import com.project.realestate.service.ArticleService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 public class ArticleServiceImpl implements ArticleService {
+
+    @Autowired
+    private ArticleRepository articleRepository;
     @Override
-    public String getHeadline() {
-        return "passing value to controller from service";
+    public List<Article> getHeadline() {
+        List<Article> headline = articleRepository.fetchHeadline();
+        return headline;
     }
 }

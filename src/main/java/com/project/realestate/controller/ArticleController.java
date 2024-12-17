fc833cf2e1de8b76e5fdb3e06009b0f5bd0ea427
Denta Muhajir <dentamuhajir@gmail.com>
2024-12-17 07:56:25 +0700

    Implement generic response

--- src/main/java/com/project/realestate/controller/ArticleController.java
+++ src/main/java/com/project/realestate/controller/ArticleController.java
@@ -1,6 +1,7 @@
 package com.project.realestate.controller;
 
 import com.project.realestate.dto.article.HeadlineArticleResp;
+import com.project.realestate.dto.response.GenericResponse;
 import com.project.realestate.model.Article;
 import com.project.realestate.service.ArticleService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,8 +21,8 @@ public class ArticleController {
     private ArticleService articleService;
     @GetMapping(value = "/headline")
     public ResponseEntity<?> getHeadlineArticle() {
-        List<HeadlineArticleResp> getHeadline = articleService.getHeadline();
-        return new ResponseEntity<>(getHeadline, HttpStatus.OK);
+        GenericResponse headline = articleService.getHeadline();
+        return new ResponseEntity<>(headline, HttpStatus.OK);
     }
     @GetMapping(value= "/seed/{totalData}")
     public  ResponseEntity<?> seedingDummyArticles(@PathVariable Integer totalData) {

5bb702f814d09af0ec3b40493788fdf2151b1c8a
Denta Muhajir <dentamuhajir@gmail.com>
2024-12-28 13:26:37 +0700

    Add Feature detail article by id

--- src/main/java/com/project/realestate/service/ArticleService.java
+++ src/main/java/com/project/realestate/service/ArticleService.java
@@ -15,4 +15,6 @@ public interface ArticleService {
     public GenericResponse getHeadline();
     public GenericResponse getListArticleByCategories();
     public void seedingArticle(Integer totalData);
+
+    public GenericResponse getArticleById(Long articleId);
 }

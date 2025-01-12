2c5b9d7ccc7b2ad9ef81da2d6e774c0c22f80ece
Denta Muhajir <dentamuhajir@gmail.com>
2025-01-12 09:38:34 +0700

    Add postComment in controller

--- src/main/java/com/project/realestate/controller/CommentController.java
+++ src/main/java/com/project/realestate/controller/CommentController.java
@@ -1,14 +1,24 @@
 package com.project.realestate.controller;
 
+import com.project.realestate.dto.comment.CommentPostReq;
+import com.project.realestate.dto.response.GenericResponse;
 import com.project.realestate.service.CommentService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("api/comment")
 public class CommentController {
     @Autowired
     private CommentService commentService;
+
+    @PostMapping(value = "/post")
+    public ResponseEntity<?> postComment(@RequestBody CommentPostReq commentPost) {
+        System.out.println(commentPost);
+        commentService.postComment(commentPost);
+        return new ResponseEntity<>("Post comment successfully", HttpStatus.OK);
+    }
 
 }

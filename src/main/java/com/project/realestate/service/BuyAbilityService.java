30804bbc473c188729323af47db2d6835a89411a
Denta Muhajir <dentamuhajir@gmail.com>
2025-01-28 12:55:06 +0700

    Update buy ability service to outputting data

--- src/main/java/com/project/realestate/service/BuyAbilityService.java
+++ src/main/java/com/project/realestate/service/BuyAbilityService.java
@@ -4,5 +4,5 @@ import com.project.realestate.dto.buyability.BuyAbilityReq;
 import com.project.realestate.dto.response.GenericResponse;
 
 public interface BuyAbilityService {
-    public void calculate(BuyAbilityReq request);
+    public GenericResponse calculate(BuyAbilityReq request);
 }
--- src/main/java/com/project/realestate/service/impl/BuyAbilityServiceImpl.java
+++ src/main/java/com/project/realestate/service/impl/BuyAbilityServiceImpl.java
@@ -1,40 +1,44 @@
 package com.project.realestate.service.impl;
 
 import com.project.realestate.dto.buyability.BuyAbilityReq;
+import com.project.realestate.dto.buyability.BuyAbilityResp;
 import com.project.realestate.dto.response.GenericResponse;
 import com.project.realestate.service.BuyAbilityService;
 import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
 import java.text.DecimalFormat;
+import java.util.List;
 
 @Service
 public class BuyAbilityServiceImpl implements BuyAbilityService {
     public static final DecimalFormat df = new DecimalFormat( "#.00" );
     @Override
-    public void calculate(BuyAbilityReq r) {
-        Double propertyPrice = r.getPropertyPrice();
-        Double downPayment = r.getDownPayment();
-        //BigDecimal loanAmount = propertyPrice.subtract(downPayment);
+    public GenericResponse calculate(BuyAbilityReq req) {
+        Double propertyPrice = req.getPropertyPrice();
+        Double downPayment = req.getDownPayment();
         Double loanAmount = propertyPrice - downPayment;
-        Float monthlyInterestRate =  r.getAnnualInterestRate().floatValue() / (12 * 100) ;
-        int totalLoanMonth = r.getLoanTenure() * 12;
+        Float monthlyInterestRate =  req.getAnnualInterestRate().floatValue() / (12 * 100) ;
+        int totalLoanMonth = req.getLoanTenure() * 12;
         // EMI formula
         double monthlyEMI = (loanAmount * monthlyInterestRate * Math.pow(1 + monthlyInterestRate, totalLoanMonth)) / (Math.pow(1 + monthlyInterestRate, totalLoanMonth) - 1);
-
         // Affordable EMI
-        double affordableEMI = r.getMonthlyIncome() * 0.4;
+        double affordableEMI = req.getMonthlyIncome() * 0.4;
 
         // Check affordability
         boolean isAffordable = monthlyEMI <= affordableEMI;
 
+        BuyAbilityResp resp = BuyAbilityResp.builder()
+                .loanAmount(loanAmount)
+                .monthlyEMI(monthlyEMI)
+                .affordableEMI(affordableEMI)
+                .isAffordable(isAffordable)
+                .build();
 
-        System.out.println("Loan Amount " + df.format(loanAmount));
-        System.out.println(" Get Annual Interest Rate" + r.getAnnualInterestRate());
-        System.out.println("Monthly Interest Rate " + monthlyInterestRate );
-        System.out.println("Monthly EMI" + monthlyEMI);
-        System.out.println("Affordable EMI %f" + df.format(affordableEMI));
-        System.out.println("is Affordable" + isAffordable);
-        //System.out.println("console log from service" + buyAbility);
+        return GenericResponse.builder()
+                .status("success")
+                .code(200)
+                .data(resp)
+                .build();
     }
 }

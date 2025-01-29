4fca666a02584955eeb50c73c7099c32d5cbc2d3
Denta Muhajir <dentamuhajir@gmail.com>
2025-01-29 12:11:57 +0700

    Update user repository

--- src/main/java/com/project/realestate/repository/UserRepository.java
+++ src/main/java/com/project/realestate/repository/UserRepository.java
@@ -3,5 +3,5 @@ package com.project.realestate.repository;
 import com.project.realestate.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository extends JpaRepository<Long, User> {
+public interface UserRepository extends JpaRepository<User, Long> {
 }

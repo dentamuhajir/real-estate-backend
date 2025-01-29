689259897b51d7a6dc1f6394b89d833b4c446b2a
Denta Muhajir <dentamuhajir@gmail.com>
2025-01-29 21:23:09 +0700

    Add error message if user already register & password doesn't match

--- src/main/java/com/project/realestate/service/UserService.java
+++ src/main/java/com/project/realestate/service/UserService.java
@@ -4,5 +4,5 @@ import com.project.realestate.dto.response.GenericResponse;
 import com.project.realestate.dto.user.RegisterReq;
 
 public interface UserService {
-    public void register(RegisterReq request);
+    public GenericResponse register(RegisterReq request);
 }

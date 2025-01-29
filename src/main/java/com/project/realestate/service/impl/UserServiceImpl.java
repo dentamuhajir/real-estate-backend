663f306aca9028d80aaa423768eb5e126a20497a
Denta Muhajir <dentamuhajir@gmail.com>
2025-01-29 21:23:09 +0700

    Add error message if user already register & password doesn't match

--- src/main/java/com/project/realestate/service/impl/UserServiceImpl.java
+++ src/main/java/com/project/realestate/service/impl/UserServiceImpl.java
@@ -1,10 +1,12 @@
 package com.project.realestate.service.impl;
 
+import com.project.realestate.dto.response.GenericResponse;
 import com.project.realestate.dto.user.RegisterReq;
 import com.project.realestate.model.User;
 import com.project.realestate.repository.UserRepository;
 import com.project.realestate.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -12,14 +14,31 @@ public class UserServiceImpl implements UserService {
     @Autowired
     private UserRepository userRepository;
     @Override
-    public void register(RegisterReq req) {
+    public GenericResponse register(RegisterReq req) {
         User checkEmailAlreadyExists = userRepository.findByEmail(req.getEmail());
 
-        System.out.println(checkEmailAlreadyExists);
+        if(checkEmailAlreadyExists != null) {
+            return GenericResponse.builder()
+                    .code(HttpStatus.CONFLICT.value()) // 409
+                    .status("Error")
+                    .message("Email already registered")
+                    .build();
+        }
 
+        if(!req.getPassword().equals(req.getConfirmPassword())) {
+            return GenericResponse.builder()
+                    .code(HttpStatus.UNAUTHORIZED.value()) // 401
+                    .status("Error")
+                    .message("Password does not match")
+                    .build();
+        }
 
-
-
+        return GenericResponse.builder()
+                .code(200)
+                .status("SUCCESS")
+                .message("")
+                .data("")
+                .build();
 
     }
 }

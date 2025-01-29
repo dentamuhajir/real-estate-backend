67273faa1d683d25335d627c37908c85c392527c
Denta Muhajir <dentamuhajir@gmail.com>
2025-01-29 14:43:08 +0700

    Add RegisterReq DTO

---
+++ src/main/java/com/project/realestate/dto/user/RegisterReq.java
@@ -0,0 +1,14 @@
+package com.project.realestate.dto.user;
+
+import jakarta.persistence.Column;
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class RegisterReq {
+    private String fullName;
+    private String email;
+    private String address;
+    private String password;
+}

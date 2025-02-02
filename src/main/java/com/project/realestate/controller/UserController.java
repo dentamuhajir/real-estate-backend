2eccb887c5f1b109d7988e0d085333a7bcec0a2b
Denta Muhajir <dentamuhajir@gmail.com>
2025-02-02 15:01:34 +0700

    Update to get file form FE

--- src/main/java/com/project/realestate/controller/UserController.java
+++ src/main/java/com/project/realestate/controller/UserController.java
@@ -1,7 +1,6 @@
 package com.project.realestate.controller;
 
 
-import com.project.realestate.dto.buyability.BuyAbilityReq;
 import com.project.realestate.dto.response.GenericResponse;
 import com.project.realestate.dto.user.RegisterReq;
 import com.project.realestate.service.UserService;
@@ -9,9 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.HashMap;
-import java.util.Map;
+import org.springframework.web.multipart.MultipartFile;
 
 @CrossOrigin(origins = "http://localhost:3000")
 @RestController
@@ -21,9 +18,24 @@ public class UserController {
     private UserService userService;
 
     @PostMapping(value = "register")
-    public ResponseEntity<?> register(@RequestBody RegisterReq bodyReq) {
-        GenericResponse resp = userService.register(bodyReq);
-        return new ResponseEntity<>(resp, HttpStatus.OK);
+    public ResponseEntity<?> register(@ModelAttribute RegisterReq bodyReq, @RequestParam("idCard")MultipartFile file) {
+        //GenericResponse resp = userService.register(bodyReq);
+
+        System.out.println("Username: " + bodyReq.getFullName());
+        System.out.println("Email: " + bodyReq.getEmail());
+        System.out.println("Password: " + bodyReq.getPassword());
+
+        // Print file details
+        if (!file.isEmpty()) {
+            System.out.println("File Name: " + file.getOriginalFilename());
+            System.out.println("File Size: " + file.getSize());
+            System.out.println("File Type: " + file.getContentType());
+        } else {
+            System.out.println("No file uploaded.");
+        }
+
+
+        return new ResponseEntity<>("resp", HttpStatus.OK);
     }
 
 }

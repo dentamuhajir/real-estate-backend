5d725e3751aa7e73ff5a0ae2272363e782767b21
Denta Muhajir <dentamuhajir@gmail.com>
2025-02-28 19:41:12 +0700

    Add AuthController

---
+++ src/main/java/com/project/realestate/controller/AuthController.java
@@ -0,0 +1,24 @@
+package com.project.realestate.controller;
+
+import com.project.realestate.jwt.JwtService;
+import com.project.realestate.model.User;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+public class AuthController {
+    private final AuthenticationManager authenticationManager;
+    private final JwtService jwtService;
+
+    public AuthController(AuthenticationManager authenticationManager, JwtService jwtService) {
+        this.authenticationManager = authenticationManager;
+        this.jwtService = jwtService;
+    }
+
+    @PostMapping("/login")
+    public String login(@RequestParam String username, @RequestParam String password) {
+        authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(username, password));
+        return jwtService.generateToken(username);
+    }
+}
